941301bd6bc858186395ccbe3f28e56a2424ad56
MrShadowRIFAT <91284619+MrShadowRIFAT@users.noreply.github.com>
2025-05-26 14:03:48 +0600

    Update Mount-FTPDrive.ps1

--- Mount-FTPDrive.ps1
+++ Mount-FTPDrive.ps1
@@ -2,7 +2,7 @@ Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
 [System.Windows.Forms.Application]::EnableVisualStyles()
 
-# ========== Theme ========== #
+# ========== Theme1 ========== #
 $theme = @{
     Dark = @{
         BackColor = [System.Drawing.Color]::FromArgb(40,44,52)
